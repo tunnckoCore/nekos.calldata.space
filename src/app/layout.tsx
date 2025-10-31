1e72b6d052edb72f6b3c6f7db0dcbaa730b9939f
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2025-10-31 06:50:53 +0200

    feat: vercel analytics
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
